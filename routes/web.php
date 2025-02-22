05f5e9502770d1a8b570351963b9f524d5d27df6
Роман Князев <ram3es92@mail.ru>
2025-02-22 19:31:15 +0300

    создал новые роуты в web.php

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\DataLogger;
-
+use App\Models\News;
+use App\Events\NewsHidden;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::middleware([DataLogger::class])->group(function () {
-//    Route::get('/logs', function () {
-//        return view('logs');
-//    });
-//});
-
 Route::middleware([DataLogger::class])->group(function () {
     Route::get('/logs', function () {
         return view('logs');
     });
+});
+
+Route::get('news/create-test', function () {
+
+    $news = new News();
+    $news->title = 'Test news title';
+    $news->body = 'Test news body';
+    $news->save();
+    return $news;
+});
+
+Route::get('/news/{id}/hide', function ($id) {
+
+    $news = News::findOrFail($id);
+
+    $news->hidden = true;
+    NewsHidden::dispatch($news);
+    $news->save();
+    return 'News hidden';
 });
