bb4cc8328bcd35e3772c5f830b2d7b72cd5b5cf5
Роман Князев <ram3es92@mail.ru>
2025-02-22 16:42:32 +0300

    изменил logs.blade.php

--- resources/views/logs.blade.php
+++ resources/views/logs.blade.php
@@ -1,87 +1,67 @@
 <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Логи</title>
+    <style>
+        td:nth-child(5),
+        td:nth-child(6) {
+            text-align: center;
+        }
+        table {
+            position: absolute;
+            border-spacing: 0;
+            border-collapse: collapse;
+            width: 70%;
+            box-shadow: 8px 4px 10px rgb(255 255 255 / 25%);
+        }
+        td, th {
+            padding: 10px;
+            border: 1px solid #282828;
+        }
+        tr:nth-child(odd) {
+            background-color: #C18787;
+        }
+    </style>
+</head>
+<body>
 
-    <!--<html style="background: #2C2820;">-->
+@php
+    use Illuminate\Support\Facades\DB;
 
-    <head>
-
-        <meta charset="UTF-8">
-
-        <title>Логи</title>
-
-        <style>
-            td:nth-child(5),
-            td:nth-child(6) {
-                text-align: center;
-            }
-
-            table {
-                position: absolute;
-                border-spacing: 0;
-                border-collapse: collapse;
-                width: 70%;
-                box-shadow: 8px 4px 10px rgba(255, 255, 255, 0.25);
-            }
-
-            td,
-            th {
-                padding: 10px;
-                border: 1px solid #282828;
-            }
-
-            tr:nth-child(odd) {
-                background-color: #C18787;
-            }
-
-                {
-                background-color: #C18787;
-            }
-        </style>
-
-    </head>
-
-    <body>
-
-        <?php
-
-try {
-
-    $db = new PDO('sqlite:C:\Users\ЕвгенийП\web\gb-laravel-lesson-1\gb-laravel8\log-service\database\database.sqlite');
-
-    $sql = "SELECT id, time, duration, ip, url, method, input FROM logs";
-
-    $statement = $db->prepare($sql);
-
-    $statement->execute();
-
-    $result_array = $statement->fetchAll();
-
-    echo "<div class=\"table\">";
-
-    echo "<table><tr><th>id</th><th>time</th><th>duration</th><th>ip</th><th>url</th><th>method</th><th>input</th></tr>";
-
-    foreach ($result_array as $result_row) {
-
-        echo "<tr>";
-        echo "<td align=\"center\">", $result_row["id"] . "</td>";
-        echo "<td align=\"center\">" . $result_row["time"] . "</td>";
-        echo "<td align=\"center\">" . $result_row["duration"] . "</td>";
-        echo "<td align=\"center\">" . $result_row["ip"] . "</td>";
-        echo "<td align=\"center\">", $result_row["url"] . "</td>";
-        echo "<td align=\"center\">", $result_row["method"] . "</td>";
-        echo "<td align=\"center\">" . $result_row["input"] . "</td>";
-        echo "</tr>";
+    try {
+        // Запрос к MySQL
+        $logs = DB::select("SELECT id, time, duration, ip, url, method, input FROM logs");
+    } catch (Exception $e) {
+        echo 'Ошибка подключения к базе данных: ' . $e->getMessage();
+        $logs = [];
     }
-    echo "</table>";
-    echo "</div>";
-} catch (PDOException $e) {
-    echo 'Ошибка при создании записи в базе данных: ' . $e->getMessage();
-}
+@endphp
 
-$db = null;
+<div class="table">
+    <table>
+        <tr>
+            <th>id</th>
+            <th>time</th>
+            <th>duration</th>
+            <th>ip</th>
+            <th>url</th>
+            <th>method</th>
+            <th>input</th>
+        </tr>
+        @foreach($logs as $log)
+            <tr>
+                <td align="center">{{ $log->id }}</td>
+                <td align="center">{{ $log->time }}</td>
+                <td align="center">{{ $log->duration }}</td>
+                <td align="center">{{ $log->ip }}</td>
+                <td align="center">{{ $log->url }}</td>
+                <td align="center">{{ $log->method }}</td>
+                <td align="center">{{ $log->input }}</td>
+            </tr>
+        @endforeach
+    </table>
+</div>
 
-?>
-
-
-    </body>
-
+</body>
 </html>
