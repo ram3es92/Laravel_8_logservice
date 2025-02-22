e2df899369b9021ec7c4b8b0810d0c23a97432c9
Роман Князев <ram3es92@mail.ru>
2025-02-22 16:41:45 +0300

    изменил Log.php

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Log extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'logs';
+
+    protected $fillable = ['time', 'duration', 'ip', 'url', 'method', 'input'];
+
+    public $timestamps = false;
 }
