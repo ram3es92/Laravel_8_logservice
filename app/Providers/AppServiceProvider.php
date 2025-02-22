f6b8ca8c81dd8346659f6e1f3903fcf0bb1fe3eb
Роман Князев <ram3es92@mail.ru>
2025-02-22 19:30:51 +0300

    связал событие и слушателя

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,15 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Events\NewsHidden;
+use App\Listeners\NewsHiddenListener;
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected $listen = [
+        Registered::class => [SendEmailVerificationNotification::class,],
+        NewsHidden::class => [NewsHiddenListener::class,],
+    ];
     /**
      * Register any application services.
      */
