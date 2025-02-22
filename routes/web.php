602b7884085232e451a6f7cb58cc0400cf4a4675
Роман Князев <ram3es92@mail.ru>
2025-02-22 15:18:48 +0300

    изменил web.php добавил роут

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\DataLogger;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware([DataLogger::class])->group(function () {
+    Route::get('/logs', function () {
+        return view('logs');
+    });
 });
