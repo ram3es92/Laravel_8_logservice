91028ecb2d6e6fa271ad7120b3057eebc3acb24f
Роман Князев <ram3es92@mail.ru>
2025-02-22 21:08:24 +0300

    изменения в моделе ньюс

--- app/Models/News.php
+++ app/Models/News.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Events\NewsHidden;
 
 class News extends Model
 {
-    //
+    protected static function booted()
+    {
+        static::updated(function ($news) {
+            if ($news->isDirty('is_hidden')) 
+            {
+                event(new NewsHidden('Иван', 'ivan@example.com'));
+            }
+        });
+    }
 }
